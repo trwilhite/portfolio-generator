bc4b846000b6a9319e154d78f93d31f043c5d9f7
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-01-19 14:28:04 -0500

    generate HTML and modularzied code

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    //this
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    //is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
